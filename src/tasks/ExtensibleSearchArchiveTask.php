3a89004f7eb97187fd25b31b649dd77d23acede5
gavynd6 <95837055+gavynd6@users.noreply.github.com>
2026-08-07 12:15:14 +1200

    Update ExtensibleSearchArchiveTask.php

--- src/tasks/ExtensibleSearchArchiveTask.php
+++ src/tasks/ExtensibleSearchArchiveTask.php
@@ -3,93 +3,89 @@
 namespace nglasl\extensible;
 
 use SilverStripe\Dev\BuildTask;
-use SilverStripe\ORM\DB;
 use SilverStripe\ORM\Queries\SQLDelete;
 use SilverStripe\ORM\Queries\SQLUpdate;
+use SilverStripe\Polyfill\PolyOutput;
+use Symfony\Component\Console\Input\InputInterface;
 
 /**
- *	This creates an archived collection of analytics for each search page.
- *	NOTE: The search analytics will be purged after this has taken place.
- *	@author Nathan Glasl <nathan@symbiote.com.au>
+ * This creates an archived collection of analytics for each search page.
+ * NOTE: The search analytics will be purged after this has taken place.
+ * 
+ * @author Nathan Glasl <nathan@symbiote.com.au>
  */
+class ExtensibleSearchArchiveTask extends BuildTask
+{
+    protected static string $commandName = 'extensible-search-archive';
 
-class ExtensibleSearchArchiveTask extends BuildTask {
+    private static string $segment = 'ExtensibleSearchArchiveTask';
 
-	private static $segment = 'ExtensibleSearchArchiveTask';
+    protected string $title = 'Extensible Search Archiving';
 
-	protected $title = 'Extensible Search Archiving';
+    protected string $description = 'This creates an archived collection of analytics for each search page.';
 
-	protected $description = 'This creates an archived collection of analytics for each search page.';
+    /**
+     * The number of analytics to archive for each search page.
+     */
+    private static int $number_to_archive = 100;
 
-	/**
-	 *	The number of analytics to archive for each search page.
-	 */
+    protected function execute(InputInterface $input, PolyOutput $output): int
+    {
+        set_time_limit(0);
 
-	private static $number_to_archive = 100;
+        $limit = static::config()->get('number_to_archive');
 
-	public function run($request) {
+        // Determine whether a search page has analytics.
+        foreach (ExtensibleSearchPage::get() as $page) {
+            $history = $page->History();
+            if ($history->exists()) {
 
-		set_time_limit(0);
+                // Instantiate an archive.
+                $archive = ExtensibleSearchArchive::create([
+                    'StartingDate' => $history->min('Created'),
+                    'EndingDate' => $history->max('Created'),
+                    'ExtensibleSearchPageID' => $page->ID,
+                ]);
+                $archive->write();
 
-		// Determine whether a search page has analytics.
+                // Determine the search page specific analytics.
+                $counter = 0;
+                foreach ($page->getHistorySummary() as $summary) {
 
-		foreach(ExtensibleSearchPage::get() as $page) {
-			$history = $page->History();
-			if($history->exists()) {
+                    // Determine whether the number of analytics to archive has been reached.
+                    if ($counter++ === $limit) {
+                        break;
+                    }
 
-				// Instantiate an archive.
+                    // Instantiate an archived search analytic, and place it in the archive.
+                    $archived = ExtensibleSearchArchived::create(
+                        $summary->toMap()
+                    );
+                    $archived->ArchiveID = $archive->ID;
+                    $archived->write();
+                }
 
-				$archive = ExtensibleSearchArchive::create(
-					array(
-						'StartingDate' => $history->min('Created'),
-						'EndingDate' => $history->max('Created'),
-						'ExtensibleSearchPageID' => $page->ID
-					)
-				);
-				$archive->write();
+                // The search analytics will be purged now that they've been archived.
+                $output->writeln("{$history->count()} Archived for page ID {$page->ID}");
 
-				// Determine the search page specific analytics.
+                $deleteQuery = new SQLDelete(
+                    'ExtensibleSearch',
+                    ['ExtensibleSearchPageID = ?' => $page->ID]
+                );
+                $deleteQuery->execute();
 
-				$counter = 0;
-				foreach($page->getHistorySummary() as $summary) {
+                // The search suggestion frequencies depend on the analytics, so these require updating.
+                $updateQuery = new SQLUpdate(
+                    'ExtensibleSearchSuggestion',
+                    ['Frequency' => 0],
+                    ['ExtensibleSearchPageID = ?' => $page->ID]
+                );
+                $updateQuery->execute();
+            }
+        }
 
-					// Determine whether the number of analytics to archive has been reached.
+        $output->writeln('Complete!');
 
-					if($counter++ === self::config()->number_to_archive) {
-						break;
-					}
-
-					// Instantiate an archived search analytic, and place it in the archive.
-
-					$archived = ExtensibleSearchArchived::create(
-						$summary->toMap()
-					);
-					$archived->ArchiveID = $archive->ID;
-					$archived->write();
-				}
-
-				// The search analytics will be purged now that they've been archived.
-
-				DB::alteration_message("{$history->count()} Archived");
-				$query = new SQLDelete(
-					'ExtensibleSearch',
-					"ExtensibleSearchPageID = {$page->ID}"
-				);
-				$query->execute();
-
-				// The search suggestion frequencies depend on the analytics, so these require updating.
-
-				$query = new SQLUpdate(
-					'ExtensibleSearchSuggestion',
-					array(
-						'Frequency' => 0
-					),
-					"ExtensibleSearchPageID = {$page->ID}"
-				);
-				$query->execute();
-			}
-		}
-		DB::alteration_message('<strong>Complete!</strong>');
-	}
-
+        return 0;
+    }
 }
